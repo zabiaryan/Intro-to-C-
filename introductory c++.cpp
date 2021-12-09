0548780b35d13113cab2e0f67fe9dae89a54ef28
Aryan Zabi <83302669+zabiaryan@users.noreply.github.com>
2021-12-08 22:36:51 -0800

    Add files via upload
    This is just for experiencing GitHub. Well-designed codes and projects are planned to be posted.

---
+++ introductory c++.cpp
@@ -0,0 +1,19 @@
+// Program    : Intro to C++
+// Author     : Zabi Aryan
+// Date       : Fall 2020
+// Description: This is a short introductory program to have the tools ready for the 
+//            : first C++ coding
+#include<iostream>
+using namespace std;
+int main()
+{
+	// Legal idetifiers
+	// An identifier is a programmer-defined name character represents variables of a program.
+	//for Variable names you can only use letters, digits, or underscores. 
+	// Example of illegal identifiers: 3dGraph and Mixture#3 
+	// Data type: integers,  integers are whole numbers such as 2, 3, 4....
+	int number; // datat type is integer and variable name is number
+	number = 5; // five is assigned to variable number
+	cout << "The value of number is " << number << endl; // five will be print out on the screen
+	return 0;
+}
